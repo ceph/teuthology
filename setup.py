8e9ad2eb6b1e08b5586715d01042e665f9ac8c08
Loic Dachary <ldachary@redhat.com>
2016-08-22 17:54:18 +0200

    openstack: require openstackclient 2.2.0
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- setup.py
+++ setup.py
@@ -78,7 +78,8 @@ setup(
                       # Babel, and installs 2.3.3, which is forbidden by
                       # python-novaclient 4.0.0
                       'python-novaclient',
-                      'python-openstackclient',
+                      # python-openstackclient == 3.0.0 fails when using a token
+                      'python-openstackclient == 2.2.0',
                       # with openstacklient >= 2.1.0, neutronclient no longer is
                       # a dependency but we need it anyway.
                       'python-neutronclient',
