0b1e6c2f01930488ab5129cc08f50c29a55e2971
Ricardo Dias <rdias@suse.com>
2016-09-23 09:54:34 +0100

    packaging: abstract packaging commands in install task
    Signed-off-by: Ricardo Dias <rdias@suse.com>

--- teuthology/packaging.py
+++ teuthology/packaging.py
@@ -123,6 +123,34 @@ def remove_package(package, remote):
         return False
     return remote.run(args=pkgcmd)
 
+def clean_repo_caches(clean_args, remote):
+    """
+    Clean repository cache
+    """
+    flavor = remote.os.package_type
+    if flavor == 'deb':
+        pkgcmd = ['DEBIAN_FRONTEND=noninteractive',
+                  'sudo',
+                  'apt-get',
+                  '-y',
+                  'clean']
+    elif flavor == 'rpm':
+        if remote.os.name != 'opensuse':
+            pkg_mng_cmd = 'yum'
+            pkg_mng_opts = '-y'
+        else:
+            pkg_mng_cmd = 'zypper'
+            pkg_mng_opts = '-n'
+
+        pkgcmd = ['sudo',
+                  pkg_mng_cmd,
+                  pkg_mng_opts,
+                  'clean',
+                  clean_args]
+    else:
+        log.error('clean_repo_caches: bad flavor ' + flavor + '\n')
+        return False
+    return remote.run(args=pkgcmd)
 
 def get_koji_task_result(task_id, remote, ctx):
     """
