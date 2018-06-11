dc2889aacf441f310dce2def68cbb61587699168
Vasu Kulkarni <vasu@redhat.com>
2018-06-10 17:10:08 -0700

    Fix unregister during final cleanup, due to stage cdn task that is not needed now.
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

--- teuthology/misc.py
+++ teuthology/misc.py
@@ -1312,7 +1312,9 @@ def get_distro(ctx):
 
 
 def cleanup(ctx):
-    unregister_rhel_systems(ctx)
+    # due to stage cdn use, this is run as internal task now
+    # unregister_rhel_systems(ctx)
+    pass
 
 
 def unregister_rhel_systems(ctx):
