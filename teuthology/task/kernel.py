cddd019955c8b0ba0e49faf869f3d808f64458f8
Rachana Patel <racpatel@redhat.com>
2019-11-12 23:55:47 -0500

    using put_file method instead of python shutil.copyfileobj
    Signed-off-by: Rachana Patel <racpatel@redhat.com>

--- teuthology/task/kernel.py
+++ teuthology/task/kernel.py
@@ -342,7 +342,7 @@ def download_kernel(ctx, config):
             # local package - src is path
             log.info('Copying kernel package {path} to {role}...'.format(
                 path=src, role=role))
-	    role_remote.put_file(src,remote_pkg_path(role_remote))    
+            role_remote.put_file(src,remote_pkg_path(role_remote))    
         else:
             # gitbuilder package - src is sha1
             log.info('Downloading kernel {sha1} on {role}...'.format(
