ceph/ceph

commit fe6bec1d42178cade0ed6d63319e4abc08f0f775

teuthology/run: do not check ceph packages if verify_ceph_hash is disabled
Signed-off-by: Kefu Chai <kchai@redhat.com>

teuthology/run.py
@@ -170,7 +170,13 @@ def get_initial_tasks(lock, config, machine_type):
     overrides = config.get('overrides', {})
     having_repos = ('repo' in config.get('install', {}) or
                     'repo' in overrides.get('install', {}))
-    if not ('redhat' in config or having_repos):
+    if 'redhat' in config:
+        pass
+    elif having_repos:
+        pass
+    elif not config.get('verify_ceph_hash', True):
+        pass
+    else:
         init_tasks += [
             {'internal.check_packages': None},
             {'internal.buildpackages_prep': None},

ceph/teuthology

commit 437a7125cd8affedb9707a274405ce0202ea1b61

teuthology/run.py: s/repo/repos/
to address the regression introduced by
fe6bec1d42178cade0ed6d63319e4abc08f0f775, which broke the `--test-repo`
option handling machinery

Signed-off-by: Kefu Chai <kchai@redhat.com>

teuthology/run.py
@@ -168,8 +168,8 @@ def validate_tasks(config):
 def get_initial_tasks(lock, config, machine_type):
     init_tasks = []
     overrides = config.get('overrides', {})
-    having_repos = ('repo' in config.get('install', {}) or
-                    'repo' in overrides.get('install', {}))
+    having_repos = ('repos' in config.get('install', {}) or
+                    'repos' in overrides.get('install', {}))
     if 'redhat' in config:
         pass
     elif having_repos: