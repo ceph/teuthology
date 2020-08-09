798557499dabb28c7e5cf53000ef60031be5be23
Shraddha Agrawal <shraddha.agrawal000@gmail.com>
2020-08-09 07:52:08 +0530

    add teuthology-dispatcher
    This commit does the following:
    
    1. a new cmd teuthology-dispatcher: It watches a queue and takes
    job from it, locks required nodes without reimaging them and runs
    the job as its suprocess by invoking teuthology-dispacther in
    supervisor mode. Supervisor mode reimages the target machines in
    the config, and invokes teuthology cmd to run the job.
    
    2. refactors  task/internal/lock_machines.py: doing so enables
    locking machines in dispatcher while following DRY.
    
    3. refactors reimaging logic in lock/ops.py: doing so enables
    reimaging machines in dispatcher's supervisor mode while following
    DRY.
    
    4. adds an argument, reimage, to lock_many in lock/ops.py: enables
    optional reimagining of machines depending on the value. Defaults
    to True. Used in dispatcher to lock machines without reimaging them.
    
    Signed-off-by: Shraddha Agrawal <shraddha.agrawal000@gmail.com>

--- setup.py
+++ setup.py
@@ -131,6 +131,7 @@ setup(
             'teuthology-describe = scripts.describe:main',
             'teuthology-reimage = scripts.reimage:main',
             'teuthology-wait = scripts.wait:main',
+            'teuthology-dispatcher = scripts.dispatcher:main',
             ],
         },
 
