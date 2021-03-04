23b963fba5b365b2859b251158e3e08e3fb85c3f
Rishabh Dave <ridave@redhat.com>
2021-03-04 16:38:37 +0530

    orchestra: move non-file transfer methods from remote.Remote
    Move methods that operate on a remote machine without depending on
    another remote machine from orchestra.remote.Remote to a different class
    in a different module. This enables applications like vstart_runner.py
    (in Ceph repo) to reuse these methods by a simple import for running
    tests locally without necessarily depending on SSH.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- teuthology/orchestra/remote.py
+++ teuthology/orchestra/remote.py
@@ -7,17 +7,14 @@ import teuthology.lock.util
 from teuthology.orchestra import run
 from teuthology.orchestra import connection
 from teuthology.orchestra import console
-from teuthology.orchestra.opsys import OS
 import teuthology.provision
 from teuthology import misc
-from teuthology.exceptions import CommandFailedError
 from teuthology.misc import host_shortname
-import errno
+from teuthology.orchestra.remote_ops import NonTransferRemoteOps
+
 import time
 import re
 import logging
-from io import BytesIO
-from io import StringIO
 import os
 import pwd
 import tempfile
@@ -26,7 +23,7 @@ import netaddr
 log = logging.getLogger(__name__)
 
 
-class Remote(object):
+class Remote(NonTransferRemoteOps):
 
     """
     A connection to a remote host.
@@ -216,110 +213,6 @@ class Remote(object):
         r.remote = self
         return r
 
-    def mkdtemp(self, suffix=None, parentdir=None):
-        """
-        Create a temporary directory on remote machine and return it's path.
-        """
-        args = ['mktemp', '-d']
-
-        if suffix:
-            args.append('--suffix=%s' % suffix)
-        if parentdir:
-            args.append('--tmpdir=%s' % parentdir)
-
-        return self.sh(args).strip()
-
-    def mktemp(self, suffix=None, parentdir=None):
-        """
-        Make a remote temporary file
-
-        Returns: the path of the temp file created.
-        """
-        args = ['mktemp']
-
-        if suffix:
-            args.append('--suffix=%s' % suffix)
-        if parentdir:
-            args.append('--tmpdir=%s' % parentdir)
-
-        return self.sh(args).strip()
-
-    def sh(self, script, **kwargs):
-        """
-        Shortcut for run method.
-
-        Usage:
-            my_name = remote.sh('whoami')
-            remote_date = remote.sh('date')
-        """
-        if 'stdout' not in kwargs:
-            kwargs['stdout'] = BytesIO()
-        if 'args' not in kwargs:
-            kwargs['args'] = script
-        proc = self.run(**kwargs)
-        out = proc.stdout.getvalue()
-        if isinstance(out, bytes):
-            return out.decode()
-        else:
-            return out
-
-    def sh_file(self, script, label="script", sudo=False, **kwargs):
-        """
-        Run shell script after copying its contents to a remote file
-
-        :param script:  string with script text, or file object
-        :param sudo:    run command with sudo if True,
-                        run as user name if string value (defaults to False)
-        :param label:   string value which will be part of file name
-        Returns: stdout
-        """
-        ftempl = '/tmp/teuthology-remote-$(date +%Y%m%d%H%M%S)-{}-XXXX'\
-                 .format(label)
-        script_file = self.sh("mktemp %s" % ftempl).strip()
-        self.sh("cat - | tee {script} ; chmod a+rx {script}"\
-            .format(script=script_file), stdin=script)
-        if sudo:
-            if isinstance(sudo, str):
-                command="sudo -u %s %s" % (sudo, script_file)
-            else:
-                command="sudo %s" % script_file
-        else:
-            command="%s" % script_file
-
-        return self.sh(command, **kwargs)
-
-    def chmod(self, file_path, permissions):
-        """
-        As super-user, set permissions on the remote file specified.
-        """
-        args = [
-            'sudo',
-            'chmod',
-            permissions,
-            file_path,
-            ]
-        self.run(
-            args=args,
-            )
-
-    def chcon(self, file_path, context):
-        """
-        Set the SELinux context of a given file.
-
-        VMs and non-RPM-based hosts will skip this operation because ours
-        currently have SELinux disabled.
-
-        :param file_path: The path to the file
-        :param context:   The SELinux context to be used
-        """
-        if self.os.package_type != 'rpm' or \
-                self.os.name in ['opensuse', 'sle']:
-            return
-        if teuthology.lock.query.is_vm(self.shortname):
-            return
-        self.run(args="sudo chcon {con} {path}".format(
-            con=context, path=file_path))
-
     def _sftp_put_file(self, local_path, remote_path):
         """
         Use the paramiko.SFTPClient to put a file. Returns the remote filename.
@@ -365,9 +258,6 @@ class Remote(object):
                 break
             file_size = file_size / 1024.0
         return "{:3.0f}{}".format(file_size, unit)
-
-    def remove(self, path):
-        self.run(args=['rm', '-fr', path])
 
     def put_file(self, path, dest_path, sudo=False):
         """
@@ -459,159 +349,6 @@ class Remote(object):
             '.',
             ])
         return self.run(args=args, wait=False, stdout=run.PIPE)
-
-    def copy_file(self, src, dst, sudo=False, mode=None, owner=None,
-                                              mkdir=False, append=False):
-        """
-        Copy data to remote file
-
-        :param src:     source file path on remote host
-        :param dst:     destination file path on remote host
-        :param sudo:    use sudo to write file, defaults False
-        :param mode:    set file mode bits if provided
-        :param owner:   set file owner if provided
-        :param mkdir:   ensure the destination directory exists, defaults False
-        :param append:  append data to the file, defaults False
-        """
-        dd = 'sudo dd' if sudo else 'dd'
-        args = dd + ' if=' + src + ' of=' + dst
-        if append:
-            args += ' conv=notrunc oflag=append'
-        if mkdir:
-            mkdirp = 'sudo mkdir -p' if sudo else 'mkdir -p'
-            dirpath = os.path.dirname(dst)
-            if dirpath:
-                args = mkdirp + ' ' + dirpath + '\n' + args
-        if mode:
-            chmod = 'sudo chmod' if sudo else 'chmod'
-            args += '\n' + chmod + ' ' + mode + ' ' + dst
-        if owner:
-            chown = 'sudo chown' if sudo else 'chown'
-            args += '\n' + chown + ' ' + owner + ' ' + dst
-        args = 'set -ex' + '\n' + args
-        self.run(args=args)
-
-    def move_file(self, src, dst, sudo=False, mode=None, owner=None,
-                                              mkdir=False):
-        """
-        Move data to remote file
-
-        :param src:     source file path on remote host
-        :param dst:     destination file path on remote host
-        :param sudo:    use sudo to write file, defaults False
-        :param mode:    set file mode bits if provided
-        :param owner:   set file owner if provided
-        :param mkdir:   ensure the destination directory exists, defaults False
-        """
-        mv = 'sudo mv' if sudo else 'mv'
-        args = mv + ' ' + src + ' ' + dst
-        if mkdir:
-            mkdirp = 'sudo mkdir -p' if sudo else 'mkdir -p'
-            dirpath = os.path.dirname(dst)
-            if dirpath:
-                args = mkdirp + ' ' + dirpath + '\n' + args
-        if mode:
-            chmod = 'sudo chmod' if sudo else 'chmod'
-            args += ' && ' + chmod + ' ' + mode + ' ' + dst
-        if owner:
-            chown = 'sudo chown' if sudo else 'chown'
-            args += ' && ' + chown + ' ' + owner + ' ' + dst
-        self.run(args=args)
-
-    def read_file(self, path, sudo=False, stdout=None,
-                              offset=0, length=0):
-        """
-        Read data from remote file
-
-        :param path:    file path on remote host
-        :param sudo:    use sudo to read the file, defaults False
-        :param stdout:  output object, defaults to io.BytesIO()
-        :param offset:  number of bytes to skip from the file
-        :param length:  number of bytes to read from the file
-
-        :raises: :class:`FileNotFoundError`: there is no such file by the path
-        :raises: :class:`RuntimeError`:      unexpected error occurred
-
-        :returns: the file contents in bytes, if stdout is `io.BytesIO`, by default
-        :returns: the file contents in str, if stdout is `io.StringIO`
-        """
-        dd = 'sudo dd' if sudo else 'dd'
-        args = dd + ' if=' + path + ' of=/dev/stdout'
-        iflags=[]
-        # we have to set defaults here instead of the method's signature,
-        # because python is reusing the object from call to call
-        stdout = stdout or BytesIO()
-        if offset:
-            args += ' skip=' + str(offset)
-            iflags += 'skip_bytes'
-        if length:
-            args += ' count=' + str(length)
-            iflags += 'count_bytes'
-        if iflags:
-            args += ' iflag=' + ','.join(iflags)
-        args = 'set -ex' + '\n' + args
-        proc = self.run(args=args, stdout=stdout, stderr=StringIO(), check_status=False, quiet=True)
-        if proc.returncode:
-            if 'No such file or directory' in proc.stderr.getvalue():
-                raise FileNotFoundError(errno.ENOENT,
-                        f"Cannot find file on the remote '{self.name}'", path)
-            else:
-                raise RuntimeError("Unexpected error occurred while trying to "
-                        f"read '{path}' file on the remote '{self.name}'")
-
-        return proc.stdout.getvalue()
-
-
-    def write_file(self, path, data, sudo=False, mode=None, owner=None,
-                                     mkdir=False, append=False):
-        """
-        Write data to remote file
-
-        :param path:    file path on remote host
-        :param data:    str, binary or fileobj to be written
-        :param sudo:    use sudo to write file, defaults False
-        :param mode:    set file mode bits if provided
-        :param owner:   set file owner if provided
-        :param mkdir:   preliminary create the file directory, defaults False
-        :param append:  append data to the file, defaults False
-        """
-        dd = 'sudo dd' if sudo else 'dd'
-        args = dd + ' of=' + path
-        if append:
-            args += ' conv=notrunc oflag=append'
-        if mkdir:
-            mkdirp = 'sudo mkdir -p' if sudo else 'mkdir -p'
-            dirpath = os.path.dirname(path)
-            if dirpath:
-                args = mkdirp + ' ' + dirpath + '\n' + args
-        if mode:
-            chmod = 'sudo chmod' if sudo else 'chmod'
-            args += '\n' + chmod + ' ' + mode + ' ' + path
-        if owner:
-            chown = 'sudo chown' if sudo else 'chown'
-            args += '\n' + chown + ' ' + owner + ' ' + path
-        args = 'set -ex' + '\n' + args
-        self.run(args=args, stdin=data, quiet=True)
-
-    def sudo_write_file(self, path, data, **kwargs):
-        """
-        Write data to remote file with sudo, for more info see `write_file()`.
-        """
-        self.write_file(path, data, sudo=True, **kwargs)
-
-    @property
-    def os(self):
-        if not hasattr(self, '_os'):
-            try:
-                os_release = self.sh('cat /etc/os-release').strip()
-                self._os = OS.from_os_release(os_release)
-                return self._os
-            except CommandFailedError:
-                pass
-
-            lsb_release = self.sh('lsb_release -a').strip()
-            self._os = OS.from_lsb_release(lsb_release)
-        return self._os
 
     @property
     def arch(self):
