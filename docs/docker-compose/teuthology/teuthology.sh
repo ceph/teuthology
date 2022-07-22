8d1d7047f6baf592036d0b74bb8da3ff554c562c
Zack Cerza <zack@redhat.com>
2022-07-22 11:51:19 -0600

    docs/docker-compose: Ensure we actually schedule
    This should have failed: https://github.com/ceph/teuthology/pull/1776/checks
    
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- docs/docker-compose/teuthology/teuthology.sh
+++ docs/docker-compose/teuthology/teuthology.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+set -e
 # We don't want -x yet, in case the private key is sensitive
 if [ -n "$SSH_PRIVKEY_FILE" ]; then
     echo "$SSH_PRIVKEY" > $HOME/.ssh/$SSH_PRIVKEY_FILE
@@ -33,6 +34,8 @@ if [ -z "$TEUTHOLOGY_WAIT" ]; then
         --force-priority \
         $CUSTOM_CONF
     DISPATCHER_EXIT_FLAG='--exit-on-empty-queue'
+    teuthology-queue -m $MACHINE_TYPE -s | \
+      python3 -c "import sys, json; assert json.loads(sys.stdin.read())['count'] > 0, 'queue is empty!'"
 fi
 teuthology-dispatcher -v \
     --log-dir /teuthology/log \
