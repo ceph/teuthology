219040fbcfb36b557637a33c629f97bd89e658ac
Zack Cerza <zack@redhat.com>
2022-08-02 12:38:27 -0600

    containers/testnode: Install podman
    To get a recent version we have to switch OSes.

--- containers/testnode/testnode_start.sh
+++ containers/testnode/testnode_start.sh
@@ -3,7 +3,7 @@ set -x
 cat /run/secrets/id_rsa.pub >> /root/.ssh/authorized_keys
 cat /run/secrets/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 chown ubuntu /home/ubuntu/.ssh/authorized_keys
-payload="{\"name\": \"$(hostname)\", \"machine_type\": \"testnode\", \"up\": true, \"locked\": false, \"os_type\": \"ubuntu\", \"os_version\": \"20.04\"}"
+payload="{\"name\": \"$(hostname)\", \"machine_type\": \"testnode\", \"up\": true, \"locked\": false, \"os_type\": \"centos\", \"os_version\": \"8.stream\"}"
 for i in $(seq 1 5); do
     echo "attempt $i"
     curl -v -f -d "$payload" http://paddles:8080/nodes/ && break
