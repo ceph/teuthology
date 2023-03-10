eb7170357938875ebbd297ee56ed6a0624ff05bb
sunjunnan <sunjunnan@cmss.chinamobile.com>
2023-03-10 09:38:37 +0000

    teuthology: add the function of finding the commit correctly when the code is not in github
    Signed-off-by: Sun Junnan <sunjunnan_yewu@cmss.chinamobile.com>
    
    add the function of finding the commit correctly when the code is not in github

--- teuthology/suite/util.py
+++ teuthology/suite/util.py
@@ -209,6 +209,8 @@ def git_validate_sha1(project, sha1, project_owner='ceph'):
         # kinda specific to knowing git.ceph.com is gitweb
         url = ('http://git.ceph.com/?p=%s.git;a=blob_plain;f=.gitignore;hb=%s'
                % (project, sha1))
+    elif config.ceph_git_base_url in url:
+        url = '/'.join((url, 'commit', sha1))
     else:
         raise RuntimeError(
             'git_validate_sha1: how do I check %s for a sha1?' % url
