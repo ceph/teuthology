38862840bf949da58bc18f820d64c73942dff8fe
Patrick Donnelly <pdonnell@redhat.com>
2023-03-22 10:48:43 -0400

    teuthology: do not compress tarballs when pulling dir
    Where we use this, it's for pulling log files that are already
    compressed. Do not waste time double compressing!
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- teuthology/misc.py
+++ teuthology/misc.py
@@ -745,8 +745,8 @@ def pull_directory(remote, remotedir, localdir, write_to=copy_fileobj):
               remote.shortname, remotedir, localdir)
     if not os.path.exists(localdir):
         os.mkdir(localdir)
-    r = remote.get_tar_stream(remotedir, sudo=True)
-    tar = tarfile.open(mode='r|gz', fileobj=r.stdout)
+    r = remote.get_tar_stream(remotedir, sudo=True, compress=False)
+    tar = tarfile.open(mode='r|', fileobj=r.stdout)
     while True:
         ti = tar.next()
         if ti is None:
