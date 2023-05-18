63ed009aad519f0c14456360a50605cb670d570a
NitzanMordhai <nmordech@redhat.com>
2023-05-18 04:57:40 +0000

    suite: fix typo osd pool default size The 'osd pool default size' config had typo, we didn't took it and actually ignored it until this fix. Since we never use it and fixing the typo will cause all suites to use it, I'll delete it completely and if we want it in the future, lets fix it then.
    Signed-off-by: Nitzan Mordechai <nmordech@redhat.com>

--- teuthology/suite/placeholder.py
+++ teuthology/suite/placeholder.py
@@ -83,7 +83,6 @@ dict_templ = {
                     'log file': '/var/log/ceph/ceph-$name.$pid.log'
                 },
                 'mon': {
-                    'osd default pool size': 2
                 }
             }
         },
