8547b70b58158b751a834d90cecf9ab3fd25dd13
John Mulligan <jmulligan@redhat.com>
2024-02-17 10:49:31 -0500

    orchestra/connection: support alternate ssh config path
    Allow the configuration to specify a `ssh_config_path` parameter.
    In my "lab" setup I populate a custom ssh-config rather than constantly
    adding and removing entries from my "real" ssh config file.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- teuthology/orchestra/connection.py
+++ teuthology/orchestra/connection.py
@@ -80,7 +80,8 @@ def connect(user_at_host, host_key=None, keep_alive=False, timeout=60,
     )
 
     key_filename = key_filename or config.ssh_key
-    ssh_config_path = os.path.expanduser("~/.ssh/config")
+    ssh_config_path = config.ssh_config_path or "~/.ssh/config"
+    ssh_config_path = os.path.expanduser(ssh_config_path)
     if not key_filename and os.path.exists(ssh_config_path):
         ssh_config = paramiko.SSHConfig()
         ssh_config.parse(open(ssh_config_path))
