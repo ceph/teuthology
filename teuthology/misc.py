ceph/teuthology

commit dbd55e37563ce0fde01e1bfa19ca2fd2c03f0194

misc._ssh_keyscan: Sort keys before returning any
ssh-keyscan's output is unsorted, so this function wasn't deterministic.

Signed-off-by: Zack Cerza <zack@redhat.com>

teuthology/misc.py
@@ -1138,9 +1138,11 @@ def _ssh_keyscan(hostname):
         line = line.strip()
         if line and not line.startswith('#'):
             log.error(line)
+    keys = list()
     for line in p.stdout:
         host, key = line.strip().decode().split(' ', 1)
-        return key
+        keys.append(key)
+    return sorted(keys)[0]
 
 
 def ssh_keyscan_wait(hostname):

commit 837a0e188680456723805204ad87a8f4098203cd

misc: allow _ssh_keyscan return None again
A host may not have any keys, so _ssh_keyscan should return None,
instead of failing with error:

    IndexError: list index out of range

Fixes: dbd55e37563ce0fde01e1bfa19ca2fd2c03f0194

Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@clyso.com>

teuthology/misc.py
@@ -1127,7 +1127,8 @@ def _ssh_keyscan(hostname):
     for line in p.stdout:
         host, key = line.strip().decode().split(' ', 1)
         keys.append(key)
-    return sorted(keys)[0]
+    if len(keys) > 0:
+        return sorted(keys)[0]
 
 
 def ssh_keyscan_wait(hostname):