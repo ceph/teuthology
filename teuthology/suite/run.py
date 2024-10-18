d05aa26b35c50545ecebeb0467ec8a68adcc65fa
Patrick Donnelly <pdonnell@redhat.com>
2024-10-18 08:44:13 -0400

    teuthology/suite: merge base_config with other fragments
    Presently the code tries to merge the base_config when the worker starts
    running. There's no need to construct it this way and it prevents sharing the
    "defaults" with the fragment merging infrastructure. It also prevents
    overriding defaults like:
    
        kernel
            branch: wip-pdonnell-i66704
            client:
                branch: wip-pdonnell-i66704
                flavor: default
                kdb: 1
                sha1: 745cacd8f31e50d7f3b6039bbd8c9a8dfc07bf03
            flavor: default
            kdb: 1
            sha1: 745cacd8f31e50d7f3b6039bbd8c9a8dfc07bf03
    
    A YAML fragment can set kernel.client but it cannot delete the defaults for
    kernel.(branch|flavor|kdb|sha1) because there's no way to remove YAML elements
    via a deep merge.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- teuthology/suite/run.py
+++ teuthology/suite/run.py
@@ -9,7 +9,6 @@ import time
 
 from humanfriendly import format_timespan
 
-from tempfile import NamedTemporaryFile
 from teuthology import repo_utils
 
 from teuthology.config import config, JobConfig
@@ -639,16 +638,9 @@ Note: If you still want to go ahead, use --job-threshold 0'''
             filter_out=self.args.filter_out,
             filter_all=self.args.filter_all,
             filter_fragments=self.args.filter_fragments,
+            base_config=self.base_config,
             seed=self.args.seed,
             suite_name=suite_name))
-
-        # create, but do not write, the temp file here, so it can be
-        # added to the args in collect_jobs, but not filled until
-        # any backtracking is done
-        base_yaml_path = NamedTemporaryFile(
-            prefix='schedule_suite_', delete=False
-        ).name
-        self.base_yaml_paths.insert(0, base_yaml_path)
 
         # compute job limit in respect of --sleep-before-teardown
         job_limit = self.args.limit or 0
@@ -714,9 +706,6 @@ Note: If you still want to go ahead, use --job-threshold 0'''
                     dry_run=self.args.dry_run,
                 )
 
-        with open(base_yaml_path, 'w+b') as base_yaml:
-            base_yaml.write(str(self.base_config).encode())
-
         if jobs_to_schedule:
             self.write_rerun_memo()
 
@@ -727,8 +716,6 @@ Note: If you still want to go ahead, use --job-threshold 0'''
         self.check_num_jobs(len(jobs_to_schedule))
 
         self.schedule_jobs(jobs_missing_packages, jobs_to_schedule, name)
-
-        os.remove(base_yaml_path)
 
         count = len(jobs_to_schedule)
         missing_count = len(jobs_missing_packages)
