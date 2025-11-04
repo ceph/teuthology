ceph/teuthology

commit fc5a3197f22ea0c18aed29ec1cc20f41ab618ee6

task/install: allow both list and dicts for extra_system_packages
Fixes: https://tracker.ceph.com/issues/73527

Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@clyso.com>

teuthology/task/install/__init__.py
@@ -9,7 +9,6 @@ from teuthology import misc as teuthology
 from teuthology import contextutil, packaging
 from teuthology.parallel import parallel
 from teuthology.task import ansible
-from teuthology.exceptions import ConfigError
 
 from distutils.version import LooseVersion
 from teuthology.task.install.util import (
@@ -417,6 +416,21 @@ def ceph_deploy_upgrade(ctx, config):
 ceph_deploy_upgrade.__doc__ = docstring_for_upgrade.format(
     cmd_parameter='ceph_deploy_upgrade')
 
+def _override_extra_system_packages(config, project, install_overrides):
+    teuthology.deep_merge(config, install_overrides.get(project, {}))
+    extra_overrides = install_overrides.get('extra_system_packages')
+    if extra_overrides:
+        extra = config.get('extra_system_packages')
+        if extra is None:
+            e = dict(deb=[], rpm=[])
+        elif isinstance(extra, list):
+            e = dict(deb=copy.deepcopy(extra), rpm=copy.deepcopy(extra))
+        elif isinstance(extra, dict):
+            e = copy.deepcopy(extra)
+        elif isinstance(extra, str):
+            e = dict(deb=[extra], rpm=[extra])
+
+        config['extra_system_packages'] = teuthology.deep_merge(e, extra_overrides)
 
 @contextlib.contextmanager
 def task(ctx, config):
@@ -568,22 +582,10 @@ def task(ctx, config):
     repos = None
 
     if overrides:
-        try:
-            install_overrides = overrides.get('install', {})
-            log.debug('INSTALL overrides: %s' % install_overrides)
-            teuthology.deep_merge(config, install_overrides.get(project, {}))
-            overrides_extra_system_packages = install_overrides.get('extra_system_packages')
-            if overrides_extra_system_packages:
-                extra_system_packages = config.get('extra_system_packages')
-                config['extra_system_packages'] = teuthology.deep_merge(extra_system_packages, overrides_extra_system_packages)
-            repos = install_overrides.get('repos', None)
-        except AssertionError:
-            raise ConfigError(
-                "'install' task config and its overrides contain" \
-                "conflicting types for the same config key. Ensure that " \
-                "the configuration is of the same type (dict, list, etc.) " \
-                "in both the task definition and its overrides."
-            )
+        install_overrides = overrides.get('install', {})
+        log.debug('INSTALL overrides: %s' % install_overrides)
+        _override_extra_system_packages(config, project, install_overrides)
+        repos = install_overrides.get('repos', None)
 
     log.debug('config %s' % config)
 

teuthology/test/task/test_install.py
@@ -3,6 +3,7 @@ import pytest
 import yaml
 
 from mock import patch, Mock
+from pytest import mark
 
 from teuthology.task import install
 
@@ -335,3 +336,85 @@ class TestInstall(object):
         with pytest.raises(RuntimeError) as e:
             install.redhat.install_pkgs(ctx, remote, version, rh_ds_yaml)
         assert "Version check failed" in str(e)
+
+    @mark.parametrize(
+        'conf, expect',
+        [
+            [
+                {
+                    'tasks': [ { 'install': { 'clean': True, }, }, ],
+                    'overrides': {
+                        'install': {
+                            'ceph': {
+                                 'extra_system_packages': ['alpha'],
+                                 'flavor': 'default',
+                                 'sha1': '0123456789abcdef0123456789abcdef01234567',
+                             },
+                            'extra_system_packages': {
+                                'deb': [],
+                                'rpm': ['xerxes', 'yellow'],
+                            },
+                        },
+                    },
+                },
+                {
+                    'deb': ['alpha'],
+                    'rpm': ['alpha', 'xerxes', 'yellow'],
+                }
+            ],
+            [
+                {
+                    'tasks': [ { 'install': { 'clean': True, }, }, ],
+                    'overrides': {
+                        'install': {
+                            'ceph': {
+                                 'flavor': 'default',
+                                 'sha1': '0123456789abcdef0123456789abcdef01234567',
+                             },
+                            'extra_system_packages': {
+                                'deb': [],
+                                'rpm': ['xerxes', 'yellow'],
+                            },
+                        },
+                    },
+                },
+                {
+                    'deb': [],
+                    'rpm': ['xerxes', 'yellow'],
+                }
+            ],
+            [
+                {
+                    'tasks': [ { 'install': { 'clean': True, }, }, ],
+                    'overrides': { 'install': {
+                            'ceph': { 'flavor': 'default', 'sha1': '012345', },
+                            'extra_system_packages': { 'rpm': ['xerxes', 'yellow'], },
+                        },
+                    },
+                },
+                { 'deb': [], 'rpm': ['xerxes', 'yellow'], }
+
+            ],
+            [
+                {
+                    'tasks': [ { 'install': { 'clean': True,
+                                             'extra_system_packages': { 'deb': ['alpha'], 'rpm': ['bravo'] },
+                                             }, }, ],
+                    'overrides': { 'install': {
+                            'ceph': { 'flavor': 'default', 'sha1': '012345', },
+                            'extra_system_packages': { 'rpm': ['xerxes', 'yellow'], },
+                        },
+                    },
+                },
+                { 'deb': ['alpha'], 'rpm': ['bravo', 'xerxes', 'yellow'], }
+
+            ],
+
+        ]
+    )
+    def test_install_extra_system_packages(self, conf, expect):
+        install_task = conf.get('tasks')[0].get('install')
+        install_overrides = conf.get('overrides').get('install', {})
+        install._override_extra_system_packages(install_task, 'ceph', install_overrides)
+
+        assert install_task.get('extra_system_packages') == expect

commit 46012fc980da2a3772b31c964a8f20c83fd102aa

task/install: extra_system_packages at the top level can be a list
Commit fc5a3197f22e ("task/install: allow both list and dicts for
extra_system_packages") introduced a regression for job definitions
where extra_system_packages at the top level (i.e. on the install
task itself) is a list.  For example

  overrides:
    install:
      ceph:
        flavor: default
        sha1: b7d57fefbec68934ae5e6e7d7c48ac09182397a5
      extra_system_packages:
      - pv

produces AssertionError on

  assert isinstance(b, dict)

in deep_merge() when it's called from _override_extra_system_packages().

Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

teuthology/task/install/__init__.py
@@ -427,6 +427,9 @@ def _override_extra_system_packages(config, project, install_overrides):
             e = dict(deb=copy.deepcopy(extra), rpm=copy.deepcopy(extra))
         elif isinstance(extra, dict):
             e = copy.deepcopy(extra)
+
+        if isinstance(extra_overrides, list):
+            extra_overrides = dict(deb=extra_overrides, rpm=extra_overrides)
         elif isinstance(extra, str):
             e = dict(deb=[extra], rpm=[extra])
 

teuthology/test/task/test_install.py
@@ -368,6 +368,28 @@ class TestInstall(object):
                     'overrides': {
                         'install': {
                             'ceph': {
+                                 'extra_system_packages': {
+                                     'deb': [],
+                                     'rpm': ['xerxes', 'yellow'],
+                                 },
+                                 'flavor': 'default',
+                                 'sha1': '0123456789abcdef0123456789abcdef01234567',
+                             },
+                            'extra_system_packages': ['alpha'],
+                        },
+                    },
+                },
+                {
+                    'deb': ['alpha'],
+                    'rpm': ['xerxes', 'yellow', 'alpha'],
+                }
+            ],
+            [
+                {
+                    'tasks': [ { 'install': { 'clean': True, }, }, ],
+                    'overrides': {
+                        'install': {
+                            'ceph': {
                                  'flavor': 'default',
                                  'sha1': '0123456789abcdef0123456789abcdef01234567',
                              },
@@ -380,6 +402,24 @@ class TestInstall(object):
                 },
                 {
                     'deb': [],
+                    'rpm': ['xerxes', 'yellow'],
+                }
+            ],
+            [
+                {
+                    'tasks': [ { 'install': { 'clean': True, }, }, ],
+                    'overrides': {
+                        'install': {
+                            'ceph': {
+                                 'flavor': 'default',
+                                 'sha1': '0123456789abcdef0123456789abcdef01234567',
+                             },
+                            'extra_system_packages': ['xerxes', 'yellow'],
+                        },
+                    },
+                },
+                {
+                    'deb': ['xerxes', 'yellow'],
                     'rpm': ['xerxes', 'yellow'],
                 }
             ],