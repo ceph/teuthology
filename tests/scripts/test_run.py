b0da69312b12d975223e06e4f71606cf8d183200
T K Chandra Hasan <t.k.chandra.hasan@ibm.com>
2026-08-04 13:52:42 +0530

    parser: Migrate scripts/run.py and teuthology/run.py from docopt to argparse
    - Migrate teuthology (scripts/run.py) from docopt to argparse
    - Fix interactive-on-error key: use hyphen form to match run_tasks.py and contextutil.py
    - Fix get_teuthology_command to reconstruct --hyphen-flag from argparse underscore keys
    - Update tests to use parse_args from scripts.run directly instead of creating a new parser
    - Update test assertions to reflect argparse key/value conventions
    
    Tracker: https://tracker.ceph.com/issues/74760
    Signed-off-by: T K Chandra Hasan <t.k.chandra.hasan@ibm.com>

--- tests/scripts/test_run.py
+++ tests/scripts/test_run.py
@@ -1,9 +1,5 @@
-import docopt
-
 from script import Script
-from scripts import run
-
-doc = run.__doc__
+from scripts.run import parse_args
 
 
 class TestRun(Script):
@@ -11,7 +7,7 @@ class TestRun(Script):
     script_module = 'scripts.run'
 
     def test_all_args(self):
-        args = docopt.docopt(doc, [
+        args = parse_args([
             "--verbose",
             "--archive", "some/archive/dir",
             "--description", "the_description",
@@ -23,24 +19,26 @@ class TestRun(Script):
             "--block",
             "--name", "the_name",
             "--suite-path", "some/suite/dir",
+            "--interactive-on-error",
             "path/to/config.yml",
         ])
-        assert args["--verbose"]
-        assert args["--archive"] == "some/archive/dir"
-        assert args["--description"] == "the_description"
-        assert args["--owner"] == "the_owner"
-        assert args["--lock"]
-        assert args["--machine-type"] == "machine_type"
-        assert args["--os-type"] == "os_type"
-        assert args["--os-version"] == "os_version"
-        assert args["--block"]
-        assert args["--name"] == "the_name"
-        assert args["--suite-path"] == "some/suite/dir"
-        assert args["<config>"] == ["path/to/config.yml"]
+        assert args.verbose
+        assert args.archive == "some/archive/dir"
+        assert args.description == "the_description"
+        assert args.owner == "the_owner"
+        assert args.lock
+        assert args.machine_type == "machine_type"
+        assert args.os_type == "os_type"
+        assert args.os_version == "os_version"
+        assert args.block
+        assert args.name == "the_name"
+        assert args.suite_path == "some/suite/dir"
+        assert args.interactive_on_error
+        assert args.config == ["path/to/config.yml"]
 
     def test_multiple_configs(self):
-        args = docopt.docopt(doc, [
+        args = parse_args([
             "config1.yml",
             "config2.yml",
         ])
-        assert args["<config>"] == ["config1.yml", "config2.yml"]
+        assert args.config == ["config1.yml", "config2.yml"]
